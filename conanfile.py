df8c6a2f16dc15175f6c43826516576d730809ed
Aleksey Timin <atimin@gmail.com>
2022-05-23 07:17:57 +0200

    release 0.5.0

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, CMake
 
 class DriftFrameworkConan(ConanFile):
     name = "reduct-cpp"
-    version = "0.4.0"
+    version = "0.5.0"
     license = "MIT"
     author = "Alexey Timin"
     url = "https://github.com/reduct-storage/reduct-cpp"
