b90a8df72d2e967a62f6af684e2038eb6b6aa23d
Kataev Victor <kataev.victor.1234@gmail.com>
2025-01-12 01:38:11 +0100

    start migration to conan2

--- cmake/InstallDependencies.cmake
+++ cmake/InstallDependencies.cmake
@@ -1,64 +1,72 @@
-if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
-            "${CMAKE_BINARY_DIR}/conan.cmake"
-            EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
-            TLS_VERIFY ON)
-endif ()
+# if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+#     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+#     file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
+#             "${CMAKE_BINARY_DIR}/conan.cmake"
+#             EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
+#             TLS_VERIFY ON)
+# endif ()
 
-include(${CMAKE_BINARY_DIR}/conan.cmake)
+# include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 find_program(CONAN_CMD conan)
-if (CONAN_CMD)
-    conan_cmake_autodetect(settings)
-    conan_cmake_install(PATH_OR_REFERENCE ${CMAKE_CURRENT_SOURCE_DIR}/conanfile.py
-            BUILD missing
-            SETTINGS ${settings})
-
-    include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-    conan_basic_setup()
+if(CONAN_CMD)
+    find_package(fmt REQUIRED)
+    find_package(nlohmann_json REQUIRED)
+    find_package(httplib REQUIRED)
+    find_package(concurrentqueue REQUIRED)
 
     add_library(dependencies INTERFACE)
-    target_link_libraries(dependencies INTERFACE ${CONAN_LIBS})
-    target_compile_definitions(dependencies INTERFACE CONAN)
-
-else ()
+    target_link_libraries(
+        dependencies
+        INTERFACE
+            fmt::fmt
+            nlohmann_json::nlohmann_json
+            httplib::httplib
+            concurrentqueue::concurrentqueue
+    )
+else()
     message(STATUS "Conan not found. Fetch dependencies")
     include(FetchContent)
     FetchContent_Declare(
-            fmt
-            URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.zip
-            URL_HASH MD5=6e20923e12c4b78a99e528c802f459ef
+        fmt
+        URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.zip
+        URL_HASH MD5=6e20923e12c4b78a99e528c802f459ef
     )
 
     FetchContent_Declare(
-            nlohmann_json
-            URL https://github.com/nlohmann/json/archive/refs/tags/v3.11.3.zip
-            URL_HASH MD5=23712ebf3a4b4ccb39f2375521716ab3
+        nlohmann_json
+        URL https://github.com/nlohmann/json/archive/refs/tags/v3.11.3.zip
+        URL_HASH MD5=23712ebf3a4b4ccb39f2375521716ab3
     )
 
     FetchContent_Declare(
-            httplib
-            URL https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.16.0.zip
-            URL_HASH MD5=c5367889819d677bd06d6c7739896b2b
+        httplib
+        URL https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.16.0.zip
+        URL_HASH MD5=c5367889819d677bd06d6c7739896b2b
     )
 
     FetchContent_Declare(
-            concurrentqueue
-            URL https://github.com/cameron314/concurrentqueue/archive/refs/tags/v1.0.4.zip
-            URL_HASH MD5=814c5e121b29e37ee836312f0eb0328f
+        concurrentqueue
+        URL
+            https://github.com/cameron314/concurrentqueue/archive/refs/tags/v1.0.4.zip
+        URL_HASH MD5=814c5e121b29e37ee836312f0eb0328f
     )
 
     add_library(dependencies INTERFACE)
     FetchContent_MakeAvailable(fmt nlohmann_json httplib concurrentqueue)
-    target_link_libraries(dependencies INTERFACE fmt nlohmann_json httplib concurrentqueue)
+    target_link_libraries(
+        dependencies
+        INTERFACE fmt nlohmann_json httplib concurrentqueue
+    )
 
     if(NOT REDUCT_CPP_USE_STD_CHRONO)
         FetchContent_Declare(
-                date
-                URL https://github.com/HowardHinnant/date/archive/refs/tags/v3.0.1.zip
-                URL_HASH MD5=cf556cc376d15055b8235b05b2fc6253)
+            date
+            URL
+                https://github.com/HowardHinnant/date/archive/refs/tags/v3.0.1.zip
+            URL_HASH MD5=cf556cc376d15055b8235b05b2fc6253
+        )
         FetchContent_MakeAvailable(date)
         target_link_libraries(dependencies INTERFACE date)
     endif()
-endif ()
+endif()
--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,14 @@
-from conans import ConanFile, CMake
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import copy
+from conan.tools.scm import Git
 
 
 class DriftFrameworkConan(ConanFile):
     name = "reduct-cpp"
-    version = "1.14.0"
+    version = "1.13.0"
     license = "MIT"
     author = "Alexey Timin"
     url = "https://github.com/reduct-storage/reduct-cpp"
@@ -11,36 +16,58 @@ class DriftFrameworkConan(ConanFile):
     topics = ("reduct-storage", "http-client", "http-api")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "cpp-httplib:with_openssl": True, "cpp-httplib:with_zlib": True,
-                       "date:header_only": True}
-    generators = "cmake"
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "cpp-httplib/*:with_openssl": True,
+        "cpp-httplib/*:with_zlib": True,
+        "date/*:header_only": True,
+    }
+    generators = "CMakeDeps", "CMakeToolchain"
 
-    requires = ("fmt/11.0.2",
-                "cpp-httplib/0.16.0",
-                "nlohmann_json/3.11.3",
-                "openssl/3.2.2",
-                "concurrentqueue/1.0.4",
-                "date/3.0.1")
+    requires = (
+        "fmt/11.0.2",
+        "cpp-httplib/0.16.0",
+        "nlohmann_json/3.11.3",
+        "openssl/3.2.2",
+        "concurrentqueue/1.0.4",
+        "date/3.0.1",
+    )
 
     def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+        if self.settings.get_safe("os") == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
 
     def source(self):
-        self.run(f'git clone --branch v{self.version} https://github.com/reduct-storage/reduct-cpp.git')
+        git = Git(self)
+        git.clone(url="https://github.com/reduct-storage/reduct-cpp.git", target=".")
+        git.checkout(f"v{self.version}")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir='reduct-cpp')
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include/reduct", src=f"{self.source_folder}/reduct-cpp/src/reduct")
-        self.copy("*reductcpp.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        copy(
+            self,
+            "*.h",
+            dst=join(self.package_folder, "include", "reduct"),
+            src=join(self.source_folder, "src", "reduct"),
+        )
+        copy(
+            self,
+            "*reductcpp.lib",
+            dst=join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(self, "*.dll", dst=join(self.package_folder, "bin"), keep_path=False)
+        copy(self, "*.so", dst=join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.dylib", dst=join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.a", dst=join(self.package_folder, "lib"), keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["reductcpp"]
