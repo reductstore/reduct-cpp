4210a36b3907d580c13caa9a8c06fdae2149bf2c
Kataev Victor <kataev.victor.1234@gmail.com>
2025-01-19 18:46:05 +0100

    Uncomment

--- cmake/InstallDependencies.cmake
+++ cmake/InstallDependencies.cmake
@@ -1,13 +1,3 @@
-# if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-#     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-#     file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
-#             "${CMAKE_BINARY_DIR}/conan.cmake"
-#             EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
-#             TLS_VERIFY ON)
-# endif ()
-
-# include(${CMAKE_BINARY_DIR}/conan.cmake)
-
 find_program(CONAN_CMD conan)
 if(CONAN_CMD)
     find_package(fmt REQUIRED)
