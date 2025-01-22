17e76a967260dbed67d111f395217d78fe2ad9ca
Victor <kataev.victor.1234@gmail.com>
2025-01-22 13:14:03 +0100

    RS-563: Migration to Conan v2 (#83)
    * start migration to conan2
    
    * fix conan package
    
    * Reformat
    
    * change conan version to 2, fix CI
    
    * fix conan version
    
    * Fix cmake preset name
    
    * Fix bash command
    
    * Add install conan action
    
    * Add action file
    
    * Add shell name
    
    * Change build-package action
    
    * Fix unit test clone
    
    * Add install
    
    * Fix conan install
    
    * Add REDUCT_CPP_USE_STD_CHRONO
    
    * Set c++ standart to 20
    
    * Add date
    
    * Remove install step from main ci
    
    * Add show preset
    
    * Add build_tests option to action
    
    * Fix build tests
    
    * Fix run tests
    
    * Check windows
    
    * Add std::chrono option logic to conanfile
    
    * Add requirements method
    
    * Add cmake build action
    
    * Install OpenSSL for windows
    
    * Update changelog
    
    * add jira ticket id
    
    ---------
    
    Co-authored-by: Alexey Timin <atimin@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,46 +1,139 @@
-from conans import ConanFile, CMake
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import copy
+from conan.tools.scm import Git
 
 
 class DriftFrameworkConan(ConanFile):
     name = "reduct-cpp"
-    version = "1.14.0"
     license = "MIT"
     author = "Alexey Timin"
     url = "https://github.com/reduct-storage/reduct-cpp"
     description = "Reduct Storage Client SDK for C++"
     topics = ("reduct-storage", "http-client", "http-api")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True, "cpp-httplib:with_openssl": True, "cpp-httplib:with_zlib": True,
-                       "date:header_only": True}
-    generators = "cmake"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "with_chrono": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_chrono": False,
+        "cpp-httplib/*:with_openssl": True,
+        "cpp-httplib/*:with_zlib": True,
+        "date/*:header_only": True,
+    }
 
-    requires = ("fmt/11.0.2",
-                "cpp-httplib/0.16.0",
-                "nlohmann_json/3.11.3",
-                "openssl/3.2.2",
-                "concurrentqueue/1.0.4",
-                "date/3.0.1")
+    requires = (
+        "fmt/11.0.2",
+        "cpp-httplib/0.16.0",
+        "nlohmann_json/3.11.3",
+        "openssl/3.2.2",
+        "concurrentqueue/1.0.4",
+    )
+
+    def set_version(self):
+        if not self.version:
+            git = Git(self, self.recipe_folder)
+            self.version = git.run("describe --tags") + ".local"
 
     def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+        if self.settings.get_safe("os") == "Windows":
+            self.options.rm_safe("fPIC")
+            self.options.with_chrono = True
+        elif (
+            self.settings.get_safe("compiler") == "gcc"
+            and self.settings.get_safe("compiler.version") >= "14"
+        ):
+            self.options.with_chrono = True
+
+    def requirements(self):
+        if not self.options.with_chrono:
+            self.requires("date/3.0.1")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        if self.options.with_chrono:
+            tc.variables["REDUCT_CPP_USE_STD_CHRONO"] = "ON"
+        tc.generate()
+
+    def export_sources(self):
+        if ".local" in self.version:
+            copy(
+                self,
+                "CMakeLists.txt",
+                src=self.recipe_folder,
+                dst=self.export_sources_folder,
+            )
+            copy(
+                self, "cmake/*", src=self.recipe_folder, dst=self.export_sources_folder
+            )
+            copy(self, "src/*", src=self.recipe_folder, dst=self.export_sources_folder)
 
     def source(self):
-        self.run(f'git clone --branch v{self.version} https://github.com/reduct-storage/reduct-cpp.git')
+        if ".local" not in self.version:
+            git = Git(self)
+            git.clone(
+                url="https://github.com/reduct-storage/reduct-cpp.git", target="."
+            )
+            git.checkout(f"v{self.version}")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir='reduct-cpp')
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include/reduct", src=f"{self.source_folder}/reduct-cpp/src/reduct")
-        self.copy("*reductcpp.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        copy(
+            self,
+            "*.h",
+            dst=join(self.package_folder, "include", "reduct"),
+            src=join(self.source_folder, "src", "reduct"),
+        )
+        copy(
+            self,
+            "*reductcpp.lib",
+            src=join(self.build_folder, "lib"),
+            dst=join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.dll",
+            src=self.build_folder,
+            dst=join(self.package_folder, "bin"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.so",
+            src=self.build_folder,
+            dst=join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.dylib",
+            src=self.build_folder,
+            dst=join(self.package_folder, "lib"),
+            keep_path=False,
+        )
+        copy(
+            self,
+            "*.a",
+            src=self.build_folder,
+            dst=join(self.package_folder, "lib"),
+            keep_path=False,
+        )
 
     def package_info(self):
         self.cpp_info.libs = ["reductcpp"]
