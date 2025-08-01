2560f69dd84bee935de18ff1ee2aada6b113f7d1
Kataev Victor <kataev.victor.1234@gmail.com>
2025-08-01 14:51:47 +0200

    fix(chore): change dependency versions

--- cmake/InstallDependencies.cmake
+++ cmake/InstallDependencies.cmake
@@ -3,7 +3,7 @@ if(REDUCT_CPP_USE_FETCHCONTENT)
     include(FetchContent)
     FetchContent_Declare(
         fmt
-        URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.zip
+        URL https://github.com/fmtlib/fmt/archive/refs/tags/9.1.0.zip
         URL_HASH MD5=6e20923e12c4b78a99e528c802f459ef
     )
 
@@ -15,8 +15,8 @@ if(REDUCT_CPP_USE_FETCHCONTENT)
 
     FetchContent_Declare(
         httplib
-        URL https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.16.0.zip
-        URL_HASH MD5=c5367889819d677bd06d6c7739896b2b
+        URL https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.14.3.zip
+        URL_HASH MD5=af82eb38506ca531b6d1d53524ff7912
     )
 
     FetchContent_Declare(
@@ -26,52 +26,50 @@ if(REDUCT_CPP_USE_FETCHCONTENT)
         URL_HASH MD5=814c5e121b29e37ee836312f0eb0328f
     )
 
-    FetchContent_Declare(
-        zlib
-        URL https://github.com/madler/zlib/releases/download/v1.3.1/zlib131.zip
-        URL_HASH MD5=ef76f7ebfd97778a6653b1d8413541c0
-    )
+    #    FetchContent_Declare(
+    #        zlib
+    #        URL https://github.com/madler/zlib/releases/download/v1.3.1/zlib131.zip
+    #        URL_HASH MD5=ef76f7ebfd97778a6653b1d8413541c0
+    #    )
 
     FetchContent_MakeAvailable(
         fmt
         nlohmann_json
         httplib
         concurrentqueue
-        zlib
+        #        zlib
     )
 
     # Add aliases
     add_library(concurrentqueue::concurrentqueue ALIAS concurrentqueue)
-    add_library(ZLIB::ZLIB ALIAS zlib)
 else()
-    find_package(fmt 11.0.2 REQUIRED)
+    find_package(fmt 9.1.0 REQUIRED)
     find_package(nlohmann_json 3.11.3 REQUIRED)
 
     # If cpp-httplib is not found via find_package() search for it using pkg-config
-    find_package(httplib 0.16.0 QUIET)
+    find_package(httplib 0.14.3 QUIET)
     if(NOT httplib_FOUND)
         message(
             STATUS
             "cpp-httplib not found via find_package(), checking pkg-config"
         )
         find_package(PkgConfig REQUIRED)
-        pkg_check_modules(httplib REQUIRED IMPORTED_TARGET cpp-httplib)
+        pkg_check_modules(httplib REQUIRED IMPORTED_TARGET cpp-httplib>=0.14.3)
         add_library(httplib::httplib ALIAS PkgConfig::httplib)
     endif()
 
     if(NOT VCPKG_ENABLED)
-        find_package(concurrentqueue 0.16.0 REQUIRED)
+        find_package(concurrentqueue 1.0.4 REQUIRED)
     else()
-        find_package(unofficial-concurrentqueue 0.16.0 REQUIRED)
+        find_package(unofficial-concurrentqueue 1.0.4 REQUIRED)
         add_library(
             concurrentqueue::concurrentqueue
             ALIAS unofficial::concurrentqueue::concurrentqueue
         )
     endif()
-    find_package(ZLIB 1.3.1 REQUIRED)
 endif()
 
-find_package(OpenSSL 3.2.2 REQUIRED)
+find_package(OpenSSL 3.0.13 REQUIRED)
 
 # Set dependencies list
 set(RCPP_DEPENDENCIES
@@ -79,7 +77,6 @@ set(RCPP_DEPENDENCIES
     nlohmann_json::nlohmann_json
     httplib::httplib
     concurrentqueue::concurrentqueue
-    ZLIB::ZLIB
     OpenSSL::SSL
     OpenSSL::Crypto
 )
